c232da5fd40d10a85fcadd6d52ebda1be4f1dac5
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-05-19 14:38:06 +0700

    public auth service pada class auth service

--- src/main/java/com/deploy/pertemuan11/service/AuthService.java
+++ src/main/java/com/deploy/pertemuan11/service/AuthService.java
@@ -1,4 +1,17 @@
 package com.deploy.pertemuan11.service;
 
+import com.deploy.pertemuan11.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
 }
